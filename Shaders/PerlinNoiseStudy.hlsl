24be89d46fdbb4543e48d5ca07a61ebaf022db03
daan <daan.detre@telenet.be>
2026-01-28 02:28:02 +0100

    damn I finished it

--- Shaders/PerlinNoiseStudy.hlsl
+++ Shaders/PerlinNoiseStudy.hlsl
@@ -1,4 +1,5 @@
-float2 PerlinHash2(float2 p)
+
+float2 Hash2(float2 p)
 {
     p = float2(
         dot(p, float2(127.1, 311.7)),
@@ -7,29 +8,37 @@ float2 PerlinHash2(float2 p)
     return frac(sin(p) * 43758.5453);
 }
 
+float2 PerlinVector(float2 cell)
+{
+    float2 g = Hash2(cell) * 2.0 - 1.0;
+    return normalize(g);
+}
+
 void PerlinNoise2D_float(float2 UV, out float Noise)
 {
     float2 cell = floor(UV);
     float2 local = frac(UV);
 
-    float CellValue00 = PerlinHash2(cell).x;
-    float CellValue10 = PerlinHash2(cell + float2(1, 0)).x;
-    float CellValue01 = PerlinHash2(cell + float2(0, 1)).x;
-    float CellValue11 = PerlinHash2(cell + float2(1, 1)).x;
+    float2 VectorValue00 = PerlinVector(cell);
+    float2 VectorValue10 = PerlinVector(cell + float2(1, 0));
+    float2 VectorValue01 = PerlinVector(cell + float2(0, 1));
+    float2 VectorValue11 = PerlinVector(cell + float2(1, 1));
 
     float2 PixelDistanceFrom00Corner = local - float2(0, 0);
     float2 PixelDistanceFrom10Corner = local - float2(1, 0);
     float2 PixelDistanceFrom01Corner = local - float2(0, 1);
     float2 PixelDistanceFrom11Corner = local - float2(1, 1);
 
-    float ProcessedValueForCorner00FromPixel = dot(CellValue00, PixelDistanceFrom00Corner);
-    float ProcessedValueForCorner10FromPixel = dot(CellValue10, PixelDistanceFrom10Corner);
-    float ProcessedValueForCorner01FromPixel = dot(CellValue01, PixelDistanceFrom01Corner);
-    float ProcessedValueForCorner11FromPixel = dot(CellValue11, PixelDistanceFrom11Corner);
+    float ProcessedValueForCorner00FromPixel = dot(VectorValue00, PixelDistanceFrom00Corner);
+    float ProcessedValueForCorner10FromPixel = dot(VectorValue10, PixelDistanceFrom10Corner);
+    float ProcessedValueForCorner01FromPixel = dot(VectorValue01, PixelDistanceFrom01Corner);
+    float ProcessedValueForCorner11FromPixel = dot(VectorValue11, PixelDistanceFrom11Corner);
 
-    float LeftToRightGradientAtPixelCell = lerp(ProcessedValueForCorner00FromPixel, ProcessedValueForCorner10FromPixel, local.x);
-    float LeftToRightGradientAbovePixelCell = lerp(ProcessedValueForCorner01FromPixel, ProcessedValueForCorner11FromPixel, local.x);
+    float2 f = local * local * local * (local * (local * 6 - 15) + 10);
 
-    Noise = lerp(LeftToRightGradientAtPixelCell, LeftToRightGradientAbovePixelCell, local.y);
+    float LeftToRightGradientAtPixelCell = lerp(ProcessedValueForCorner00FromPixel, ProcessedValueForCorner10FromPixel, f.x);
+    float LeftToRightGradientAbovePixelCell = lerp(ProcessedValueForCorner01FromPixel, ProcessedValueForCorner11FromPixel, f.x);
+
+    Noise = lerp(LeftToRightGradientAtPixelCell, LeftToRightGradientAbovePixelCell, f.y);
 }
 
